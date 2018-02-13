66a6c3dceabd560ab724c06c95fd77caaf7ce439
cyrille.davin <cyrille.davin@ASTPB0TCMH2.office.pbmsprod.pvt>
2018-02-13 10:57:27 +0100

    20181302: no change except SB

--- src/main/java/com/cysomel/backend/routes/blueTooth.java
+++ src/main/java/com/cysomel/backend/routes/blueTooth.java
@@ -3,15 +3,17 @@ package com.cysomel.backend.routes;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Component;
 
 @Component
 public class blueTooth extends RouteBuilder {
-	
+	  
 	@Override
 	public void configure() throws Exception {
 		 
-		//restConfiguration().host("localhost").port(8080);
+		restConfiguration().host("localhost").port(8080);
 		rest("/test")
 		 .get()
 			.produces("text/plain")
@@ -19,8 +21,8 @@ public class blueTooth extends RouteBuilder {
 			.routeId("WsTester")
 			.process(new Processor() {
 				public void process(Exchange exch) throws Exception {
-				exch.getIn().setHeader("charset","UTF-8");
-				exch.getIn().setBody("Hello World ! ;)");
+					exch.getIn().setHeader("charset","UTF-8");
+					exch.getIn().setBody("[backend] I'm alive ;)");
 				}
 			});   
 	}
